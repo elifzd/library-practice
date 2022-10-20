812c49728de9a519043ffeee93f5b5a49b296f59
elnurakhundov <elnurakhundov55@gmail.com>
2022-10-20 09:21:22 +0200

    update

---
+++ src/test/java/com/cydeo/library/step_definitions/MyStepdefs.java
@@ -0,0 +1,9 @@
+package com.cydeo.library.step_definitions;
+
+import io.cucumber.java.en.Then;
+
+public class MyStepdefs {
+    @Then("account holder name should be {string}")
+    public void accountHolderNameShouldBe(String arg0) {
+    }
+}
